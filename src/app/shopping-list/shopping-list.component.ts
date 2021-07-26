8ed0dbcf7784a71c863bcfd5e98276c4aa8ac635
ariya-mms <ariya.mms@gmail.com>
2021-07-26 23:49:51 +0430

    Bootstrap shopping list components

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {IngredientModel} from "../shared/ingredient.model";
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: IngredientModel[] = [
+    new IngredientModel("Apples", 3),
+    new IngredientModel("Tomatoes", 5)
+  ];
 
   constructor() { }
 

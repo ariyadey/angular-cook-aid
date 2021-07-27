99af0d317650a9e521240d6ccf913d056bfa8271
ariya-mms <ariya.mms@gmail.com>
2021-07-27 18:15:40 +0430

    Enhance dropdown directive using HostBinding decorative

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,21 +1,13 @@
-import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
+import {Directive, HostBinding, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
-  readonly element;
-  open = false;
-
-  constructor(elRef: ElementRef, private renderer: Renderer2) {
-    this.element = elRef.nativeElement;
-  }
+  @HostBinding("class.open") open = false;
 
   @HostListener("click")
   toggleOpen() {
-    this.open
-      ? this.renderer.removeClass(this.element,"open")
-      : this.renderer.addClass(this.element, "open");
     this.open = !this.open;
   }
 }

035e382b862c7acc65afad9f0f75153b09656eb9
ariya-mms <ariya.mms@gmail.com>
2021-07-28 22:25:28 +0430

    Redirect main page to recipes page

--- src/app/app-routes/app-routes.module.ts
+++ src/app/app-routes/app-routes.module.ts
@@ -5,6 +5,7 @@ import {RecipesComponent} from "../recipes/recipes.component";
 import {ShoppingListComponent} from "../shopping-list/shopping-list.component";
 
 const appRoutes: Routes = [
+  { path: "", redirectTo: "recipes", pathMatch: "full"},
   { path: "recipes", component: RecipesComponent },
   { path: "shopping-list", component: ShoppingListComponent },
 ];

f572cc04046fd73badae55e4123a755fb974f744
ariya-mms <ariya.mms@gmail.com>
2021-07-28 22:15:52 +0430

    Enhance header navigation using routes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,6 @@
 import {
   Component,
-  EventEmitter,
-  OnInit,
-  Output
+  OnInit
 } from '@angular/core';
 
 @Component({
@@ -11,7 +9,6 @@ import {
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  @Output() navigate = new EventEmitter<string>();
 
   constructor() { }
 
